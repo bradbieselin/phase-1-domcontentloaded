fa711313c233232e8ae0335c05a27ff679f10679
Brad Bieselin <bradley.bieselin@gmail.com>
2022-04-08 13:39:26 -0400

    commit tree 35a68e46550917a3632f1b5b218ebb042d07da8d

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    let text = document.querySelector("p");
+    console.log("Text before: " + text)
+    text.textContent = "This is really cool!";
+    console.log("Text now: " + text)
+})
